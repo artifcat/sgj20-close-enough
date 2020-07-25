24fd1f51edf37542d80eab84eaaeb726ebd16551
DeathTost <deathtost@gmail.com>
2020-07-25 17:34:47 +0200

    Add basic BirdyFlap logic

---
+++ close-enough/Assets/Scripts/Flying.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    public float velocity = 1;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * velocity;
+        }
+    }
+}
---
+++ close-enough/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer = 0;
+    public GameObject[] obstacles;
+    public float deathTime = 15;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject obs = Instantiate(obstacles[Random.Range(0, obstacles.Length)]);
+            obs.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(obs, deathTime);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
