e6cf8500824967e022fd228ea445a42d69a24afc
DeathTost <deathtost@gmail.com>
2020-07-25 19:25:21 +0200

    Add ugly parallax scrolling background and TextMeshPro Text

---
+++ close-enough/Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    public float scrollingSpeed;
+    private Rigidbody2D rb;
+    private BoxCollider2D groundCollider;
+    private float groundHorizontalLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-scrollingSpeed, 0);
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundHorizontalLength = groundCollider.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -groundHorizontalLength)
+        {
+            Vector3 offset = new Vector3(groundHorizontalLength * 2f, 0, 0);
+            transform.position = transform.position + offset;
+        }
+    }
+}
